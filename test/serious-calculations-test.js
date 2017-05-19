85829afe53bda9e7fa65b18e4f97b001295fe977
qser <karntor@163.com>
2017-05-19 11:10:08 +0800

    update4 data
    update4 data commit

--- test/serious-calculations-test.js
+++ test/serious-calculations-test.js
@@ -3,7 +3,7 @@ var assert = require('assert'),
     seriousCalculations = require('../');
 
 vows.describe('serious-calculations').addBatch({
-  'When performing serious calculations': {
+  'When performing serious calculations_d20170519': {
     topic: seriousCalculations.performSeriousCalculations(4),
     'result should be valid': function (result) {
       assert.isNumber(result);
